37d9cf8e9e1f26863cbac0ecdc0ec40d1b8ceecc
pacs <iztaccihuatl@mudhead.se>
2011-11-03 21:16:00 +0100

    Added a json transformer.
    Added l2json script that will translate the html output to json.

---
+++ l2json.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/env perl
+
+use warnings;
+
+my $first_site = 1;
+print "{\n";
+while (<>)
+{
+  chomp;
+  if (/Meny vecka (\d+) \(([\d-]+) &mdash; ([\d-]+)/)
+  {
+    my $week_num = $1;
+    my $start_date = $2;
+    my $end_date = $3;
+    $end_date = substr($start_date, 0, 5) . $end_date;
+    print "  \"week\" : \"$week_num\",\n";
+    print "  \"start\" : \"$start_date\",\n";
+    print "  \"end\" : \"$end_date\"";
+  }
+  elsif (/<h2><a id="(\w+)"/) # new day
+  {
+    $first_site = 1;
+    print ",\n";
+    print "  \"$1\" : [\n";
+  }
+  elsif (/<\/table>/)
+  {
+    print "\n  ]";
+  }
+  elsif (/<tr class="\w+"><th>(.*?)<\/th>.*?<li>(.*)<\/li><\/ul>/)
+  {
+    my $site = $1;
+    my $lunch = $2;
+    $site =~ s/&nbsp;/ /g;
+    if (not $first_site)
+    {
+      print ",\n";
+    }
+    else
+    {
+      $first_site = 0;
+    }
+    print "    { \"$site\" : [ ";
+
+    my @lunch_list = split(/<\/li><li>/, $lunch);
+    foreach (@lunch_list) # using default $_ works since this is last in while loop
+    {
+      s/[\s]{2,}/ /g; # remove multi spaces
+      s/&amp;/&/g;
+      s/&ouml;/ö/g;
+      s/&#246;/ö/g;
+      s/&Ouml;/Ö/g;
+      s/&#214;/Ö/g;
+      s/&auml;/ä/g;
+      s/&#228;/ä/g;
+      s/&Auml;/Ä/g;
+      s/&#196;/Ä/g;
+      s/&aring;/å/g;
+      s/&#229;/å/g;
+      s/&Aring;/Å/g;
+      s/&eacute;/é/g;
+      s/&#233;/é/g;
+    }
+    print join(', ', map { '{"lunch" : "' . $_ . '"}'} @lunch_list);
+
+    print " ] }";
+  }
+}
+print "\n";
+print "}\n";
