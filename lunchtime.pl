7530fb7c4138229293d480a915b1eb3395010066
pacs <cotopaxi@mudhead.se>
2012-04-30 13:11:22 +0200

    lt change for css
    The file lunchtime.css was renamed, but the generated html page
    still had the old name. Now changed to lt.css

--- lunchtime.pl
+++ lunchtime.pl
@@ -113,7 +113,7 @@ print qq{<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
 <title>Lunch time</title>
-<link rel="stylesheet" type="text/css" href="static/lunchtime.css" />
+<link rel="stylesheet" type="text/css" href="static/lt.css" />
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 <script type="text/javascript">
 <!--  
