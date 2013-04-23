3a7fb402fe5b3ef652c6c3b94fc496644698d57b
pacs <iztaccihuatl@mudhead.se>
2013-04-23 17:07:54 +0200

    Removed hojdpunkten
    Since hojdpunkten no longer lists the meny for the entire week, but
    instead only daily, it has been removed from lunchtime.

--- lunchtime.pl
+++ lunchtime.pl
@@ -13,7 +13,7 @@ getopts('f:');
 
 %urls = (
   'http://www.finninn.com/dagens.html', [\&finninn_day, \&weeknumtest, "Finn&nbsp;Inn"]
- ,'http://www.restauranghojdpunkten.se/index.php?page=Meny', [\&hojdpunkten_day, \&weeknumtest, "Höjdpunkten"]
+#,'http://www.restauranghojdpunkten.se/index.php?page=Meny', [\&hojdpunkten_day, \&weeknumtest, "Höjdpunkten"]
  ,'http://www.restaurant.ideon.se/', [\&ideonalfa_day, \&weeknumtest, "Ideon&nbsp;Alfa"]
  ,'http://www.yourvismawebsite.com/sarimner-restauranger-ab/restaurang-hilda/lunch-meny/svenska', [\&sarimner_day, \&weeknumtest, "Särimner&nbsp;Hilda"]
  ,'http://www.magnuskitchen.se/', [\&magnus_day, \&weeknumtest, "Magnus&nbsp;Kitchen"]
