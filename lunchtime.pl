e2e3940fdb01d86c88584eff17aabd7aaad0d197
pacs <cotopaxi@mudhead.se>
2017-02-06 17:45:38 +0100

    Scotland yard now in swedish
    The swedish menu is now available after the english. The regex is
    updated to find the swedish variant.

--- lunchtime.pl
+++ lunchtime.pl
@@ -438,7 +438,7 @@ sub scotlandyard_day
 {
   my ($htmlbody, $day) = @_;
   my $lunch = '';
-  if ($htmlbody =~ /(?:<strong>|<p>|<br \/>).*?$day(?:<\/strong>|<br \/>)(.+?)(?:<strong>|<\/p>|<p>)/)
+  if ($htmlbody =~ / sve.*?(?:<strong>|<p>|<br \/>).*?$day(?:<\/strong>|<br \/>)(.+?)(?:<strong>|<\/p>|<p>)/)
   {
     $lunch = $1;
     $lunch =~ s/<br \/>/ :: /g;
