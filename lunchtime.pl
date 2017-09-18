80b590bee0db629372f22a0eaa617ae485831c1d
pacs <cotopaxi@mudhead.se>
2017-09-18 10:17:31 +0200

    Scotland Yard fix
    The language tag can apparently be either Swe or swe. The problem is
    that the text 'swe' can also appear in other contexts (for instance
    sweet). Now the tag needs to appear after the week number with space
    separation.

--- lunchtime.pl
+++ lunchtime.pl
@@ -446,7 +446,7 @@ sub scotlandyard_day
 {
   my ($htmlbody, $day) = @_;
   my $lunch = '';
-  if ($htmlbody =~ /Swe.*?(?:<strong>|<p>|<br \/>).*?$day(?:<\/strong>|<br \/>)(.+?)(?:<strong>|<\/p>|<p>)/)
+  if ($htmlbody =~ /\d\s+[Ss]we.*?(?:<strong>|<p>|<br \/>).*?$day(?:<\/strong>|<br \/>)(.+?)(?:<strong>|<\/p>|<p>)/)
   {
     $lunch = $1;
     $lunch =~ s/<br \/>/ :: /g;
